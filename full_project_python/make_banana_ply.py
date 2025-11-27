69d718e2ca8ba170f98a815041a023c5a28a305a
ahmedgalai <ahmedg369@gmail.com>
2025-11-27 17:47:08 +0100

    repo cleanup

--- full_project_python/make_banana_ply.py
+++
@@ -1,5 +0,0 @@
-import open3d as o3d
-m = o3d.io.read_triangle_mesh("Banana.stl")
-o3d.io.write_triangle_mesh("Banana.ply", m)
-m = o3d.io.read_triangle_mesh("Football.stl")
-o3d.io.write_triangle_mesh("Football.ply", m)
