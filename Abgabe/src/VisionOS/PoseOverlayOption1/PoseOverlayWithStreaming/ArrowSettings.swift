cf8d2f2b2551a63e7015a265a95305449b867b8a
ahmedgalai <ahmedg369@gmail.com>
2025-12-23 12:22:31 +0100

    organization

--- Abgabe/src/VisionOS/PoseOverlayOption1/PoseOverlayWithStreaming/ArrowSettings.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-@MainActor
-final class ArrowSettings: ObservableObject {
-    @Published var color: Color = .red
-    @Published var roiColor: Color = .cyan          // fully opaque default
-    @Published var roiRadius: CGFloat = 120
-}
